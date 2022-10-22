dfdaa019bcf4426e38f9adacf87d3fda69383626
Andrey <engineer.lab09@gmail.com>
2022-10-22 22:17:34 +0300

    Added variant 17 for lab 21-23

---
+++ Lab_21_23/Variant_17/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Variant_17
+{
+	internal static class Client
+	{
+		public static void Main(string[] args)
+		{
+			Printer printer = new BW100();
+			printer.ShowPrint("Hello, World!");
+			printer = new BA215();
+			printer.ShowPrint("Hello, Europe!");
+			
+		}
+	}
+}
---
+++ Lab_21_23/Variant_17/Printer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Variant_17
+{
+	public enum Color
+	{
+		BlackWhite, FullColor
+	}
+
+	public abstract class Printer
+	{
+		protected string Model { get; }
+		protected Color Color { get; }
+
+		protected Printer(string model, Color color)
+		{
+			Model = model;
+			Color = color;
+		}
+
+		public abstract void ShowPrint(string message);
+	}
+
+	// ReSharper disable once InconsistentNaming
+	public class BW100 : Printer
+	{
+		public BW100() : base(
+			model: "BW-100", color: Color.BlackWhite) { }
+
+		public override void ShowPrint(string message)
+		{
+			Console.WriteLine($"Принтер {Model} | {Color} распечатал:\n\t" +
+			                  $"{message}");
+		}
+	}
+	// ReSharper disable once InconsistentNaming
+	public class BA215 : Printer
+	{
+		public BA215() : base(
+			model: "BA-215", color: Color.FullColor) { }
+
+		public override void ShowPrint(string message)
+		{
+			Console.WriteLine($"Принтер {Model} | {Color} распечатал:\n\t" +
+			                  $"{message}");
+		}
+	}
+}
